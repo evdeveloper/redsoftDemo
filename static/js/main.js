86285c011fac70bbc6788fcb8b57198f0da2db5a
Evgeny <mushnikovz@mail.ru>
2021-01-13 22:22:19 +0300

    new commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,43 +1,46 @@
-document.addEventListener('DOMContentLoaded', function(e) {
+document.addEventListener('DOMContentLoaded', function() {
 
-let buttons = document.querySelectorAll('.Button__buy');
+    const buttons = document.querySelectorAll('.Button__buy');
+    const btnsStateFromLS = localStorage.getItem("btnsState");
+    const btnsState = btnsStateFromLS ? JSON.parse(btnsStateFromLS) : {};
 
-function saveStorage() {
-    let btnClass = localStorage.getItem('class');
-    if (btnClass) {
-        document.documentElement.classList.add('success');
+    const setStateForButtons = () => {
+        for (let prop in btnsState) {
+            document.getElementById(prop).dataset.active = btnsState[prop];
+            document.getElementById(prop).textContent = btnsState[prop] ? "В корзине" : "Купить";
+        }
     }
-    localStorage.getItem('img');
-}
 
-saveStorage();
+    setStateForButtons();
 
-buttons.forEach(button => {
-    button.addEventListener('click', function(e){
-        this.textContent = '';
-        let spinner = document.createElement('div');
-        const check = "<img src='static/images/img/svg/check.svg'>";
-        spinner.classList.add('spinner');
-        button.appendChild(spinner);
-        fetch('https://jsonplaceholder.typicode.com/posts/1')
-        .then(response=> {
-            if(response.ok) {
-                button.classList.add('success');
-                this.innerHTML = check + 'В корзине';
-                
+    buttons.forEach(button => {
+        button.addEventListener('click', function (e) {
+            const id = e.target.id;
+            
+            if (typeof btnsState[id] === "boolean") {
+                btnsState[id] = !btnsState[id];
+            } else {
+                btnsState[id] = true;
             }
-            if (this.classList.contains('success')) {
-                localStorage.setItem('btnClass', true);
-                return;
-            }
-            localStorage.removeItem('btnClass');
-            return response.json();
-        }).then(data=> {
-            console.log(data);
-        }).catch(error=> {
-            console.log(error);
-        })
-    },false)
-})
+            
+            this.textContent = '';
+            let spinner = document.createElement('div');
+            spinner.classList.add('spinner');
+            button.appendChild(spinner);
+            fetch('https://jsonplaceholder.typicode.com/posts/1')
+                .then(response => {
+                    if (response.ok) {
+                        setStateForButtons();
+                        localStorage.setItem("btnsState", JSON.stringify(btnsState));
+                    }
+
+                    return response.json();
+                }).then(data => {
+                    console.log(data);
+                }).catch(error => {
+                    console.log(error);
+                })
+        }, false)
+    })
 
 });
